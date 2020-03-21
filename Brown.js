3de000584cbafe9bceecafd77b44b33b6e4c81e4
SmartArrow008 <59789213+SmartArrow008@users.noreply.github.com>
2020-03-21 11:55:32 +0530

    Add files via upload

---
+++ Brown.js
@@ -0,0 +1,18 @@
+class Brown extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      
+    }
+    display(){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      rectMode(CENTER);
+     fill("brown");
+      rect( 0, 0, this.width, this.height);
+      pop();
+    }
+  
+  };
+  
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# c-23_project
+# AngryBirdsStage4
